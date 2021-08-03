91b32259aa5520af289d9fb51c1d4b37fee88212
Thomas Norris <tom.norris135@gmail.com>
2021-08-03 15:44:24 -0400

    add assistant

---
+++ assistant.js
@@ -0,0 +1,42 @@
+
+var http = require('http');
+var _assistant = function(settings) {
+    this.settings = {
+        url: settings.url,
+        endpoint: settings.endpoint,
+        auth: settings.auth
+    };
+
+    this.fn = {
+        send: function (command, cb, cb_on_error) {
+            var options = url.parse(this.settings.url + this.settings.endpoint + '/' + encodeURIComponent(command));
+            options.headers = {
+                'X-Auth': this.settings.auth
+            };
+
+            var req = http.request(options, function (res) {
+                res.on('data', function (data) {
+                    console.log('Assistant Response: ' + data);
+                    if (typeof cb == 'function') {
+                        cb(data);
+                    }
+                });
+
+                res.on('close', function (data) {
+                    console.log('Connection closed.');
+                });
+            });
+
+            req.on('error', function (err) {
+                console.log('Assistant error: ' + err);
+                if (typeof cb == 'function' && cb_on_error) {
+                    cb(err);
+                }
+            });
+
+            req.end();
+        }
+    }
+};
+
+exports.assistant = _assistant;
