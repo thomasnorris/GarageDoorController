87a2eda8e81fecfaeb9fe2ba72057a9229387eba
Thomas Norris <tom.norris135@gmail.com>
2021-08-03 16:58:12 -0400

    added wifi module

---
+++ wifi.js
@@ -0,0 +1,87 @@
+var _wifi = function (settings) {
+    var wifi = require('Wifi');
+    var self = this;
+
+    this.settings = {
+        // required
+        host_name: settings.host_name,
+        ssid: settings.ssid,
+        pw: settings.pw,
+        retry_ms: settings.retry_ms,
+        first_connection_cb: settings.first_connection_cb,
+        // optional
+        led: {
+            enable_toggle: settings.led.enable_toggle || false,
+            gpio: settings.led.gpio || undefined,
+            high_value: settings.led.high_value || 1,
+            blink_interval_ms: settings.led.blink_interval_ms || 100
+        }
+    };
+
+    this.connection_info = undefined;
+    this.led_blink_interval = 0;
+
+    this.fn = {
+        init: function () {
+            wifi.setHostname(self.settings.host_name);
+            wifi.disconnect();
+
+            wifi.on('disconnected', function() {
+                console.log('Wifi disconnected, reconnecting in ' + self.settings.retry_ms + ' ms...');
+                setTimeout(function () {
+                    self.fn.connect();
+                }, self.settings.retry_ms);
+            });
+        },
+        connect: function () {
+            if (self.settings.led.enable_toggle) {
+                clearInterval(self.led_blink_interval);
+                self.led_blink_interval = self.fn.toggleLed();
+            }
+
+            console.log('Connecting wifi...');
+            wifi.connect(self.settings.ssid, {
+                password: self.settings.pw
+            }, self.fn.afterConnect);
+        },
+        afterConnect: function (err) {
+            self.connection_info = wifi.getIP();
+            var ip = self.connection_info.ip;
+
+            if (err) {
+                console.log('Wifi connection error: ' + err);
+                wifi.disconnect();
+            }
+            else if (!ip || ip == '0.0.0.0') {
+                console.log('Invalid Wifi IP.');
+                wifi.disconnect();
+            }
+            else {
+                console.log("Wifi connected! Info: " + JSON.stringify(self.connection_info));
+                if (self.settings.toggle_led) {
+                    clearInterval(self.led_blink_interval);
+                    digitalWrite(self.settings.led_gpio_pin, self.settings.led_high_value);
+                }
+
+                wifi.stopAP();
+
+                if (typeof self.settings.first_connection_cb == 'function') {
+                    self.settings.connection_cb();
+                    self.settings.first_connection_cb = undefined;
+                }
+            }
+        },
+        toggleLed: function () {
+            var state = 1;
+            return setInterval(function () {
+                digitalWrite(self.settings.led.gpio, state);
+                state = !state;
+            }, self.settings.led.blink_interval_ms);
+        }
+    };
+
+    // init
+    this.fn.init();
+};
+
+exports.wifi = _wifi;
